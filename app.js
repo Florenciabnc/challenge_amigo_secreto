9e1f80b703f8d0006accd5faebb838cea447378c
Florenciabnc <florencia.banicevich@gmail.com>
2025-02-24 16:17:01 -0300

    Funcion para agregar amigos a la lista

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let amigos = [];
+
+function agregarAmigo() {
+    let nombreUsuario = document.getElementById('amigo').value;
+    console.log(nombreUsuario);
+    if (nombreUsuario == "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(nombreUsuario);
+        document.getElementById('amigo').value = "";
+    }
+    console.log(amigos);
+}
