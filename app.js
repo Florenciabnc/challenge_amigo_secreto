181bb4dbb7ebc95202c8cda6e4d040982718d40a
Florenciabnc <florencia.banicevich@gmail.com>
2025-02-24 16:17:44 -0300

    Función 'recorrerAmigo()' para mostrar la lista de amigos

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ function agregarAmigo() {
     }
     console.log(amigos);
 }
+
+function recorrerAmigo() {
+    let lista = document.getElementById('ListaAmigos');
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li'); 
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
